5115fdbbc64b549d45abcd95ec3fc59a447328a2
Madhan D <154608700+Madhan-D249@users.noreply.github.com>
2025-05-06 10:09:22 +0530

    Create disk_usage.sh

---
+++ script2/disk_usage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+THRESHOLD=80
+USAGE =$(df /grep/ awk '{print $5}' /sed 's/%//g' 
+if ["$USAGE -gt $THRESHOLD"]
+then 
+  echo" warning: disk usage is above $THRESHOLD % its usage is $usage% now | mail -s "disk usage alert " madhandeva249@gamil.com"
+else
+  echo" disk usage is within limit:$USAGE%"
+fi
