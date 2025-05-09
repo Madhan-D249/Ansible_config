620fcf6edf7b16ed089011e9c34ec8f7e0db02a5
madhan-d249 <madhandeva249@gmail.com>
2025-05-09 08:38:15 +0530

    commi1

--- scripts/for_loops.sh
+++ scripts/for_loops.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-for i in { 1 .. 10}
+for i in {1..10}
 do 
    echo "$i"
+done
+
+
+#!/bin/bash
+#odd numbers
+for i in {1..50..2}
+do
+  echo "$i"
+done
+
+#!/bin/bash
+#even numbers
+for i in {0..50..2}
+do
+  echo "$i"
 done
