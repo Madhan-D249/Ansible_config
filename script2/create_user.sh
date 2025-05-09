39abee4acd8042df0730bd34d0b56154388ae071
Madhan D <154608700+Madhan-D249@users.noreply.github.com>
2025-05-09 22:03:17 +0530

    Update create_user.sh

--- script2/create_user.sh
+++ script2/create_user.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 read -p "enter urname: username"
+if id "$username: &>/dev/null:
+then 
+echo "user is already exists
+else
 if sudo adduser $username
 then
   echo "user created $username successfully"
